12abf304682197910b0f841e4148958d0a4e49e7
nastiagiannoudi1 <cs02664@uoi.gr>
2024-10-22 21:25:56 +0300

    Completed lab02.s for MYY505 assignment

--- lab02.s
+++ lab02.s
@@ -1,18 +1,33 @@
-
 .data
-
-array: .word 1, 0, 1, 12, 0, 1, 4
+array: .word 1, 0, 1, 12, 0, 1, 4   
 
 .text
 
-    la a0, array
-    li a1, 7    # unsigned
-    li a2, 1
+    la a0, array           
+    li a1, 7               
+    li a2, 1               
+
 prog:
-#-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
-#-----------------------------
+    li s0, 0               
+    li t0, 0               
+    beq a1, zero, done 
+
+loop:
+    beq t0, a1, done       
+    slli t1, t0, 2      
+    add t1, a0, t1      
+    lw t2, 0(t1)           
+    beq t2, a2, match  
+
+    addi t0, t0, 1         
+    j loop                 
+
+match:
+    mv s0, t1              
+    addi t0, t0, 1         
+    j loop                 
+
 done:
-    addi a7, zero, 10 
+    
+    addi a7, zero, 10      
     ecall
